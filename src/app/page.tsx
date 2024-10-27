93d8ad8c33167c9418093eab1b1490153ce5f244
Văn Thắng <vanthang23032001@gmail.com>
2024-10-28 00:48:26 +0700

    fix: resolved hydration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
+"use client";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  const handleClick = () => {
+    setIsDisabled(true);
+  }
+
+  useEffect(() => {
+    console.log(isDisabled);
+  }, [isDisabled]);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-4xl font-bold">Gomes Base</h1>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        {
+          isDisabled ? <Button disabled>Login Disabled</Button> : <Button onClick={handleClick}>Login</Button>
+        }
       </footer>
     </div>
   );
